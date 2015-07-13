9b063fb814bb10f1e91d472d7e9e9d1ce1ccfd5d
Ancil <ancilm2000@yahoo.com>
2015-07-13 13:52:10 +0200

    Initial Commit

---
+++ CourseraListDemo-ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CourseraListDemo-ObjC
+//
+//  Created by Ancil on 7/13/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CourseraListDemo-ObjCTests/CourseraListDemo_ObjCTests.m
@@ -0,0 +1,40 @@
+//
+//  CourseraListDemo_ObjCTests.m
+//  CourseraListDemo-ObjCTests
+//
+//  Created by Ancil on 7/13/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CourseraListDemo_ObjCTests : XCTestCase
+
+@end
+
+@implementation CourseraListDemo_ObjCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
